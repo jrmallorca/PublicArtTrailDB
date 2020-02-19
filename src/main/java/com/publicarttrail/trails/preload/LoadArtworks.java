1e90628b05054c9190507219dee38926785c2110
Rithika George <hp18117@bristol.ac.uk>
2020-02-19 10:43:51 +0000

    changed order

--- src/main/java/com/publicarttrail/trails/preload/LoadArtworks.java
+++ src/main/java/com/publicarttrail/trails/preload/LoadArtworks.java
@@ -41,21 +41,21 @@ class LoadArtworks implements CommandLineRunner {
                 ts.getTrailById(1),
                 as.imgToBase64("error_image.png")));
 
-        log.info("Preloading artwork: Physics Building");
+        log.info("Preloading artwork: Follow Me");
         as.add(new Artwork(
-                "Physics Building",
-                "George Oatlay",
-                "This gothic style building -HH Wills Physics Laboratory to give is its full title – was designed by Sir George Oatlay, " +
-                        "who also designed many University edifices including Wills Memorial Building at the top of Park Street. It was the first part of " +
-                        "a conceived series of the gardens under their foundations!\n"
+                "Follow Me",
+                "Jeppe Hein",
+                "In 2009 a new public sculpture by internationally acclaimed artist, Jeppe Hein, was unveiled as part of the University’s centenary celebrations. " +
+                        "Entitled “Follow Me’, the work is permanently sited in Royal Fort Gardens. The artwork was produced by Bristol-based public art producers, " +
+                        "Situations.\n\n"
                         +
-                        "The building was opened in 1927 and used materials that Harry Wills stipulated should be of “such quality that no repairs are " +
-                        "requires for 50 years; hence bronze window frames to avoid rust... the best brick with cement mortar; and Keen’s cement plaster... " +
-                        "as it does not flake like lime”.\n",
-                51.458470,
-                -2.602058,
+                        "The artwork comprises a square labyrinth of 76 vertical polished steel plates sited at the base of an incline leading down from Royal Fort " +
+                        "House. Visitors are encouraged to enter the labyrinth to experience the effect of the work. Jeppe Hein was inspired by the history of the " +
+                        "gardens-particularly the designs of 18th-century landscape Repton.\n\n",
+                51.457620,
+                -2.602613,
                 ts.getTrailById(1),
-                as.imgToBase64("physics_building.png")));
+                as.imgToBase64("follow_me.png")));
 
         log.info("Preloading artwork: Hollow");
         as.add(new Artwork(
@@ -74,42 +74,6 @@ class LoadArtworks implements CommandLineRunner {
                 ts.getTrailById(1),
                 as.imgToBase64("hollow.png")));
 
-        log.info("Preloading artwork: Follow Me");
-        as.add(new Artwork(
-                "Follow Me",
-                "Jeppe Hein",
-                "In 2009 a new public sculpture by internationally acclaimed artist, Jeppe Hein, was unveiled as part of the University’s centenary celebrations. " +
-                        "Entitled “Follow Me’, the work is permanently sited in Royal Fort Gardens. The artwork was produced by Bristol-based public art producers, " +
-                        "Situations.\n\n"
-                        +
-                        "The artwork comprises a square labyrinth of 76 vertical polished steel plates sited at the base of an incline leading down from Royal Fort " +
-                        "House. Visitors are encouraged to enter the labyrinth to experience the effect of the work. Jeppe Hein was inspired by the history of the " +
-                        "gardens-particularly the designs of 18th-century landscape Repton.\n\n",
-                51.457620,
-                -2.602613,
-                ts.getTrailById(1),
-                as.imgToBase64("follow_me.png")));
-
-        log.info("Preloading artwork: Lizard");
-        as.add(new Artwork(
-                "Lizard",
-                "Metalgnu",
-                "N/A",
-                51.458830,
-                -2.600851,
-                ts.getTrailById(1),
-                as.imgToBase64("lizard.png")));
-
-        log.info("Preloading artwork: Vertical Garden");
-        as.add(new Artwork(
-                "Vertical Garden",
-                "N/A",
-                "N/A",
-                51.458858,
-                -2.600813,
-                ts.getTrailById(1),
-                as.imgToBase64("vertical_garden.png")));
-
         log.info("Preloading artwork: Royal Fort House");
         as.add(new Artwork(
                 "Royal Fort House",
@@ -126,6 +90,7 @@ class LoadArtworks implements CommandLineRunner {
                 ts.getTrailById(1),
                 as.imgToBase64("royal_fort_house.png")));
 
+
         log.info("Preloading artwork: Metal Owl");
         as.add(new Artwork(
                 "Metal Owl",
@@ -135,5 +100,46 @@ class LoadArtworks implements CommandLineRunner {
                 -2.602257,
                 ts.getTrailById(1),
                 as.imgToBase64("owl.png")));
+
+        log.info("Preloading artwork: Physics Building");
+        as.add(new Artwork(
+                "Physics Building",
+                "George Oatlay",
+                "This gothic style building -HH Wills Physics Laboratory to give is its full title – was designed by Sir George Oatlay, " +
+                        "who also designed many University edifices including Wills Memorial Building at the top of Park Street. It was the first part of " +
+                        "a conceived series of the gardens under their foundations!\n"
+                        +
+                        "The building was opened in 1927 and used materials that Harry Wills stipulated should be of “such quality that no repairs are " +
+                        "requires for 50 years; hence bronze window frames to avoid rust... the best brick with cement mortar; and Keen’s cement plaster... " +
+                        "as it does not flake like lime”.\n",
+                51.458470,
+                -2.602058,
+                ts.getTrailById(1),
+                as.imgToBase64("physics_building.png")));
+
+        log.info("Preloading artwork: Vertical Garden");
+        as.add(new Artwork(
+                "Vertical Garden",
+                "N/A",
+                "N/A",
+                51.458858,
+                -2.600813,
+                ts.getTrailById(1),
+                as.imgToBase64("vertical_garden.png")));
+
+        log.info("Preloading artwork: Lizard");
+        as.add(new Artwork(
+                "Lizard",
+                "Metalgnu",
+                "N/A",
+                51.458830,
+                -2.600851,
+                ts.getTrailById(1),
+                as.imgToBase64("lizard.png")));
+
+
+
+
+
     }
 }
